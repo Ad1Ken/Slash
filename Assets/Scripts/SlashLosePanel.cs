e29357fb2653381ff571ea1b79d372cb8def0479
Aditya Kumar <56013517+Ad1Ken@users.noreply.github.com>
2024-08-15 02:41:33 +0530

    Basic Scripts

---
+++ Assets/Scripts/SlashLosePanel.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SlashLosePanel : MonoBehaviour
+{
+    #region PUBLIC_PROPERTIES
+    #endregion
+
+    #region PRIVATE_PROPERTIES
+    #endregion
+
+    #region UNITY_CALLBACKS
+    #endregion
+
+    #region PUBLIC_METHODS
+    #endregion
+
+    #region PRIVATE_METHODS
+    #endregion
+
+    #region DELEGTE_CALLBACKS
+    #endregion
+
+    #region Coroutines
+    #endregion
+}
